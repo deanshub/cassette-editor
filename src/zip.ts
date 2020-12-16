fc7762b57d572d29f149d835a79d6df3f40c0490
Dean Shub <deanshub@gmail.com>
2020-12-16 11:34:14 +0200

    fix escaping

--- src/zip.ts
+++ src/zip.ts
@@ -76,7 +76,7 @@ export async function zipFiles(zip: Zip) {
         ...file.data,
         response: {
           ...file.data.response,
-          body: btoa(unescape(file.data.response.body)),
+          body: btoa(escape(file.data.response.body)),
         },
       })
     );
